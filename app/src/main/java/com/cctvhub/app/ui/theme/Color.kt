ead122aedad7c03b817e1f86fce94dc84e55b4a8
ambikasoni0802-ai <ambikasoni0802@gmail.com>
2026-09-09 09:29:19 +0530

    Create Color.kt

---
+++ app/src/main/java/com/cctvhub/app/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.cctvhub.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val BgDeep = Color(0xFF0A0E14)
+val BgSurface = Color(0xFF12161F)
+val BgCard = Color(0xFF1A1F2B)
+val AccentCyan = Color(0xFF00E5C7)
+val AccentViolet = Color(0xFF7C5CFF)
+val AccentPink = Color(0xFFFF4D8D)
+val TextPrimary = Color(0xFFEAF0F5)
+val TextSecondary = Color(0xFF8A93A3)
+val StatusOnline = Color(0xFF29E07C)
+val StatusOffline = Color(0xFFFF5D5D)
